0714664ae682484324dad89f614a4cedc380a5ac
Dilshod Rahmatov <dilshod.rahmatov@gmail.com>
2023-11-17 10:52:53 +0100

    js basics

---
+++ test.js
@@ -0,0 +1,86 @@
+//console.log("Hello World!");
+//const a = 5;
+//a = 10;
+//Data Types//
+//console.log(typeof undefined);
+
+//console.log(typeof 0);
+
+//console.log(typeof true);
+
+//console.log(typeof "50");
+
+//console.log(typeof 50);
+
+//console.log(typeof Math);
+
+//console.log(typeof ["1", "2", 5]); // [ ] - array
+
+//console.log(typeof {}); //{} - object
+
+// const a = 5;
+// const b = 5;
+// const c = a + b;
+// console.log(c); //10
+
+// const f = "5"; //5
+
+// const d = f + c; //510
+
+// console.log(typeof d);
+
+//console.log(5 + 5);
+
+//const a = "Bob";
+//const b = "Tom";
+//console.log(a + b);
+
+//Conditional Expressions
+// if (x > 7) {
+//   console.log("Correct");
+// } else {
+//   console.log("Error");
+// }
+//const x = "6";
+//x > 7 ? console.log("Correct") : console.log("Error");
+//x !== 6 ? console.log("Correct") : console.log("Error");
+
+// let txt = "a ";
+
+// let length = txt.length;
+
+// console.log(length);
+
+//let str = "Apple, Banana, Kiwi";
+
+//let part = str.slice(7, 13);
+
+//let part = str.slice(-12, -6);
+
+// let str = "     hello ";
+// //console.log(str.toUpperCase());
+// console.log(str.trim());
+
+// const str = `Test Hello`;
+
+// const part = str.split(" ")[1];
+
+// console.log(part);
+// let text = "Please visit Microsoft!";
+// console.log(text);
+// let newText = text.replace("Microsoft", "DCI");
+
+// console.log(newText);
+
+const str = "The quick brown fox jumps over the lazy dog.";
+
+const words = str.split(" ");
+console.log(typeof words);
+
+// const chars = str.split('');
+// console.log(chars[8]);
+
+const array = ["fsfd", "asdfasd", "asfasdf"];
+
+const strCopy = str.split();
+//console.log(strCopy);
